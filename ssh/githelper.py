2bb16e0f1498db7db6af0d010ff9855199b19d27
jsevans <jsevans@jhome>
2023-09-24 19:24:33 +0200

    unified to one script

--- ssh/githelper.py
+++ ssh/githelper.py
@@ -1,27 +1,69 @@
 #!/usr/bin/python3
 
+import argparse
 import subprocess
 
-repoList = ()
+def list_repos(sshServer, sshUser):
+    repoStr = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "ls")
+    print(repoStr)
 
-# get list of repos and put them into a collection
-repoStr = subprocess.getoutput("ssh git@example.com 'ls | grep \.git'")
+def clone_repo(cloneRepo, sshServer, sshUser):
+    cloneOutput = subprocess.getoutput("git clone" + " " + sshUser + "@" + sshServer + ":~/" + cloneRepo + ".git") # clones the repo
+    print(cloneOutput) # prints output confirming that it was cloned
 
-# Convert the string to a list
-def Convert(string):
-    li = list(string.split("\n"))
-    return li
-  
-# create the list
-repoList = ((Convert(repoStr)))
+def new_repo(newRepo, sshServer, sshUser):
+    mkRepoOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "git init --bare " + newRepo + ".git")
+    print(mkRepoOutput)
 
-# Print the list
-for (i, item) in enumerate(repoList, start=0):
-    print(i, item)
+def archive_repo(archiveRepo, sshServer, sshUser):
+    print("Archiving " + archiveRepo + " to a tarball")
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "tar cfvz " + archiveRepo + ".tgz " + archiveRepo + ".git/")
 
-# ask for the repo number
-cloneReq = input("Which Repo? \n")
-cloneReq = int(cloneReq)
-cloneRepo = (repoList[cloneReq])
-cloneOutput = subprocess.getoutput("git clone git@example.com:~/" + cloneRepo) # clones the repo
-print(cloneOutput) # prints output confirming that it was cloned
+def remove_repo(rmRepo, sshServer, sshUser):
+    print("Deleting " + rmRepo)
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " rm -rfv " + rmRepo + ".git") # deletes the repo
+    print(cloneOutput) # prints output confirming that it was deleted
+
+def main():
+    parser = argparse.ArgumentParser(description='A simple script with multiple command-line flags.')
+
+    # Add the arguments for using your specific server
+    parser.add_argument('--server', help='What server should I use?')
+    parser.add_argument('--user', help='What user should I use?')
+
+    # Add optional flags
+    parser.add_argument('--list', '-l', action='store_true', help='Lists existing repos')
+    parser.add_argument('--clone', '-c', action='store', help='Clones a repo locally')
+    parser.add_argument('--new', '-n', action='store', help='Create a new repo')
+    parser.add_argument('--archive', '-a', action='store', help='Compresses a repo into a tarball file')
+    parser.add_argument('--remove', action='store', help='Deletes a repo')
+    
+    args = parser.parse_args()
+
+    if args.list:
+        sshServer = args.server
+        sshUser = args.user
+        list_repos(sshServer, sshUser)
+    elif args.clone:
+        sshServer = args.server
+        sshUser = args.user
+        cloneRepo = args.clone
+        clone_repo(cloneRepo, sshServer, sshUser)
+    elif args.new:
+        sshServer = args.server
+        sshUser = args.user
+        newRepo = args.new
+        new_repo(newRepo, sshServer, sshUser)
+    elif args.archive:
+        sshServer = args.server
+        sshUser = args.user
+        archiveRepo = args.archive
+        archive_repo(archiveRepo, sshServer, sshUser)
+    elif args.remove:
+        sshServer = args.server
+        sshUser = args.user
+        rmRepo = args.remove
+        remove_repo(rmRepo, sshServer, sshUser)
+
+if __name__ == '__main__':
+    main()
---
+++ ssh/old-scripts/githelper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import subprocess
+
+repoList = ()
+
+# get list of repos and put them into a collection
+repoStr = subprocess.getoutput("ssh git@example.com 'ls | grep \.git'")
+
+# Convert the string to a list
+def Convert(string):
+    li = list(string.split("\n"))
+    return li
+  
+# create the list
+repoList = ((Convert(repoStr)))
+
+# Print the list
+for (i, item) in enumerate(repoList, start=0):
+    print(i, item)
+
+# ask for the repo number
+cloneReq = input("Which Repo? \n")
+cloneReq = int(cloneReq)
+cloneRepo = (repoList[cloneReq])
+cloneOutput = subprocess.getoutput("git clone git@example.com:~/" + cloneRepo) # clones the repo
+print(cloneOutput) # prints output confirming that it was cloned
