d0aa63caab59d81ea2be93c7ded0c5e6afe42e08
Jason Evans <jsevans@mailfence.com>
2024-03-02 14:41:39 +0100

    added an option for declaring a port

--- ssh/githelper.py
+++ ssh/githelper.py
@@ -3,25 +3,29 @@
 import argparse
 import subprocess
 
-def list_repos(sshServer, sshUser):
-    repoStr = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "ls" + " " + "| sed -e 's/\.git//g'")
+def list_repos(sshServer, sshUser, sshPort):
+    repoStr = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "ls" + " " + "| sed -e 's/\.git//g'")
     print(repoStr)
 
-def clone_repo(cloneRepo, sshServer, sshUser):
-    cloneOutput = subprocess.getoutput("git clone" + " " + sshUser + "@" + sshServer + ":~/" + cloneRepo + ".git") # clones the repo
+# def clone_repo(cloneRepo, sshServer, sshUser, sshPort):
+#     cloneOutput = subprocess.getoutput("git clone" + " " + "ssh://" + sshUser + "@" + sshServer + ":~/" + cloneRepo + ".git") # clones the repo
+#     print(cloneOutput) # prints output confirming that it was cloned
+
+def clone_repo(cloneRepo, sshServer, sshUser, sshPort):
+    cloneOutput = subprocess.getoutput("git" + " " + "clone" + " " + "ssh://" + sshUser + "@" + sshServer + ":" + sshPort +  "/~/" + cloneRepo + ".git") # clones the repo
     print(cloneOutput) # prints output confirming that it was cloned
 
-def new_repo(newRepo, sshServer, sshUser):
-    mkRepoOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "git init --bare " + newRepo + ".git")
+def new_repo(newRepo, sshServer, sshUser, sshPort):
+    mkRepoOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "git init --bare " + newRepo + ".git")
     print(mkRepoOutput)
 
-def archive_repo(archiveRepo, sshServer, sshUser):
+def archive_repo(archiveRepo, sshServer, sshUser, sshPort):
     print("Archiving " + archiveRepo + " to a tarball")
-    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "tar cfvz " + archiveRepo + ".tgz " + archiveRepo + ".git/")
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer  + " " + "-p" + sshPort + " " + "tar cfvz " + archiveRepo + ".tgz " + archiveRepo + ".git/")
 
-def remove_repo(rmRepo, sshServer, sshUser):
+def remove_repo(rmRepo, sshServer, sshUser, sshPort):
     print("Deleting " + rmRepo)
-    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " rm -rfv " + rmRepo + ".git") # deletes the repo
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + " rm -rfv " + rmRepo + ".git") # deletes the repo
     print(cloneOutput) # prints output confirming that it was deleted
 
 def main():
@@ -37,25 +41,27 @@ def main():
     parser.add_argument('--new', '-n', action='store', help='Create a new repo')
     parser.add_argument('--archive', '-a', action='store', help='Compresses a repo into a tarball file')
     parser.add_argument('--remove', action='store', help='Deletes a repo')
-    
+    parser.add_argument('--port', '-p', default="22", help='Set the ssh port to something other than 22')
+
     args = parser.parse_args()
     sshServer = args.server
     sshUser = args.user
+    sshPort = args.port
 
     if args.list:
-        list_repos(sshServer, sshUser)
+        list_repos(sshServer, sshUser, sshPort)
     elif args.clone:
         cloneRepo = args.clone
-        clone_repo(cloneRepo, sshServer, sshUser)
+        clone_repo(cloneRepo, sshServer, sshUser, sshPort)
     elif args.new:
         newRepo = args.new
-        new_repo(newRepo, sshServer, sshUser)
+        new_repo(newRepo, sshServer, sshUser, sshPort)
     elif args.archive:
         archiveRepo = args.archive
-        archive_repo(archiveRepo, sshServer, sshUser)
+        archive_repo(archiveRepo, sshServer, sshUser, sshPort)
     elif args.remove:
         rmRepo = args.remove
-        remove_repo(rmRepo, sshServer, sshUser)
+        remove_repo(rmRepo, sshServer, sshUser, sshPort)
 
 if __name__ == '__main__':
     main()
