141dad001d07500b2d310ff39af2fad22a351242
Jason Evans <jason.evans@logmanager.com>
2024-08-26 13:48:15 +0200

    added dir option

--- ssh/githelper.py
+++ ssh/githelper.py
@@ -3,33 +3,33 @@
 import argparse
 import subprocess
 
-def list_repos(sshServer, sshUser, sshPort):
-    repoStr = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "ls" + " " + "| sed -e 's/\\.git//g'")
+def list_repos(sshServer, sshDir, sshUser, sshPort):
+    repoStr = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "ls" + " " + sshDir + "/" + " " + "| sed -e 's/\\.git//g'")
     print(repoStr)
 
-def rename_repo(sshServer, sshUser, sshPort, newRepo, oldRepo):
-    renameRepo = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "mv -v " + oldRepo + ".git " + newRepo + ".git")
+def rename_repo(sshServer, sshDir, sshUser, sshPort, newRepo, oldRepo):
+    renameRepo = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "mv -v " + sshDir + '/' + oldRepo + ".git " + sshDir + '/' + newRepo + ".git")
     print(renameRepo)
 
-def fork_repo(sshServer, sshUser, sshPort, newRepo, oldRepo):
-    forkRepo = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "cp -rv " + oldRepo + ".git " + newRepo + ".git")
+def fork_repo(sshServer, sshDir, sshUser, sshPort, newRepo, oldRepo):
+    forkRepo = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "cp -rv " + sshDir + '/' + oldRepo + ".git " + sshDir + '/' + newRepo + ".git")
     print(forkRepo)
 
-def clone_repo(cloneRepo, sshServer, sshUser, sshPort):
-    cloneOutput = subprocess.getoutput("git" + " " + "clone" + " " + "ssh://" + sshUser + "@" + sshServer + ":" + sshPort +  "/~/" + cloneRepo + ".git") # clones the repo
+def clone_repo(cloneRepo, sshServer, sshDir, sshUser, sshPort):
+    cloneOutput = subprocess.getoutput("git" + " " + "clone" + " " + "ssh://" + sshUser + "@" + sshServer + ":" + sshPort + " " + sshDir + '/' + cloneRepo + ".git") # clones the repo
     print(cloneOutput) # prints output confirming that it was cloned
 
-def new_repo(newRepo, sshServer, sshUser, sshPort):
-    mkRepoOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "git init --bare " + newRepo + ".git")
+def new_repo(newRepo, sshServer, sshDir, sshUser, sshPort):
+    mkRepoOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + "git init --bare " + sshDir + '/' + newRepo + ".git")
     print(mkRepoOutput)
 
-def archive_repo(archiveRepo, sshServer, sshUser, sshPort):
+def archive_repo(archiveRepo, sshServer, sshDir, sshUser, sshPort):
     print("Archiving " + archiveRepo + " to a tarball")
-    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer  + " " + "-p" + sshPort + " " + "tar cfvz " + archiveRepo + ".tgz " + archiveRepo + ".git/")
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer  + " " + "-p" + sshPort + " " + "tar cfvz " + archiveRepo + ".tgz " + sshDir + '/' + archiveRepo + ".git/")
 
-def remove_repo(rmRepo, sshServer, sshUser, sshPort):
+def remove_repo(rmRepo, sshServer, sshDir, sshUser, sshPort):
     print("Deleting " + rmRepo)
-    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + " rm -rfv " + rmRepo + ".git") # deletes the repo
+    cloneOutput = subprocess.getoutput("ssh " + sshUser + "@" + sshServer + " " + "-p" + sshPort + " " + " rm -rfv " + sshDir + '/' + rmRepo + ".git") # deletes the repo
     print(cloneOutput) # prints output confirming that it was deleted
 
 def main():
@@ -50,34 +50,36 @@ def main():
     parser.add_argument('--fork', '-f', action='store_true', help='Copy repo')
     parser.add_argument('--oldrepo', action='store', help='Old Name (Used with fork or rename)')
     parser.add_argument('--newrepo', action='store', help='New Name (Used with fork or rename)')
+    parser.add_argument('--dir', '-d', default=".", action='store', help='Set the directory where your git repos are located on the server')
 
     args = parser.parse_args()
     sshServer = args.server
     sshUser = args.user
     sshPort = args.port
+    sshDir = args.dir
     newRepo = args.newrepo
     oldRepo = args.oldrepo
     rename = args.rename
     fork = args.fork
 
     if args.list:
-        list_repos(sshServer, sshUser, sshPort)
+        list_repos(sshServer, sshDir, sshUser, sshPort)
     elif args.clone:
         cloneRepo = args.clone
-        clone_repo(cloneRepo, sshServer, sshUser, sshPort)
+        clone_repo(cloneRepo, sshServer, sshDir, sshUser, sshPort)
     elif args.new:
         newRepo = args.new
-        new_repo(newRepo, sshServer, sshUser, sshPort)
+        new_repo(newRepo, sshServer, sshDir, sshUser, sshPort)
     elif args.archive:
         archiveRepo = args.archive
-        archive_repo(archiveRepo, sshServer, sshUser, sshPort)
+        archive_repo(archiveRepo, sshServer, sshDir, sshUser, sshPort)
     elif args.remove:
         rmRepo = args.remove
-        remove_repo(rmRepo, sshServer, sshUser, sshPort)
+        remove_repo(rmRepo, sshServer, sshDir, sshUser, sshPort)
     elif args.rename:
-        rename_repo(sshServer, sshUser, sshPort, newRepo, oldRepo)
+        rename_repo(sshServer, sshDir, sshUser, sshPort, newRepo, oldRepo)
     elif args.fork:
-        fork_repo(sshServer, sshUser, sshPort, newRepo, oldRepo)
+        fork_repo(sshServer, sshDir, sshUser, sshPort, newRepo, oldRepo)
 
 if __name__ == '__main__':
     main()
